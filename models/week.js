886016b370eff31724b05f8fcec2747ce6e42cf0
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-14 11:01:26 -0500

    adds Week model

---
+++ models/week.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema;
+      
+var weekSchema = new Schema({
+    name : { type: String, required: true, trim: true,
+index: { unique: true } }
+  , description : { type: String, required: true }
+  , date_created : { type: Date, required: true, default: Date.now
+}
+});
+      
+var week = mongoose.model('week', workoutSchema);
+      
+module.exports = {
+  Week: week
+};
