bfadec0602035b1c32d31b40e3482dbff9ef2a60
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-14 10:52:20 -0500

    express.use to get

--- routes/weeks.js
+++ routes/weeks.js
@@ -1,9 +1,3 @@
-var express = require('express');
-var router = express.Router();
-
-/* Responds with json */
-router.get('/', function(req, res) {
-  res.json(200, { message: "My first route"});
-})
-
-module.exports = router;
+exports.index = function(req, res) {
+  res.json(200, { message: "My first route"} );
+}
